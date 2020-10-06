a818f8221dcaf5ae38aba36d3abf984662af8e9a
Nazokat Isamova <nazokatardasher@macbook-pro.home>
2020-10-06 15:57:25 -0600

    gats

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
 import Layout from "../components/layout"
-import Pricingcard from "../components/pricing-card"
 import Portfoliocard from "../components/portfolio-card"
 import { Container, Row, Col } from 'react-bootstrap'
 
