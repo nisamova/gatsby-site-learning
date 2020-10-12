ad46aa8c5d11ba2081b42918eae2d96264735d73
Nazokat Isamova <nazokatardasher@macbook-pro.home>
2020-10-11 22:22:18 -0600

    Improvements to SEO component

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,72 +1,66 @@
 import React from "react"
-import { Helmet }from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
+function SEO({ description, lang, meta, title, pathname }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            keywords
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+  const metaDescription = description || site.siteMetadata.description
+  const canonical = pathname ? `${site.siteMetadata.siteUrl}${pathname}` : null
 
-export const Seo = ({ title, description, keywords, image, url }) =>{
-    return ( 
-        <StaticQuery
-            query={detailsQuery}
-            render={ data=> {
-                const metaTitle = title || data.site.siteMetadata.title
-                const metaDescription = description || data.site.siteMetadata.description
-                const metaKeywords = keywords || data.site.siteMetadata.keywords
-                const metaImage = image || data.site.siteMetadata.image
-                const metaUrl = url || data.site.siteMetadata.url
-
-                    return(
-                        < Helmet 
-                        title={title}
-                        meta={[
-                            {
-                                name:`description`,
-                                content: metaDescription,
-                            },
-                            {
-                                property: `og:title`,
-                                content:metaTitle,
-                            },
-                            {
-                                property: `og:description`,
-                                content:metaDescription,
-                            },
-                            {
-                                property: `og:type`,
-                                content: `website`,
-                            },
-                            {
-                                property: `og:image`,
-                                content:metaImage,
-                            },
-                            {
-                                property: `og:url`,
-                                content:metaUrl,
-                            },
-                        ].concat(
-                            metaKeywords && metaKeywords.length > 0
-                              ? {
-                                  name: `keywords`,
-                                  content: metaKeywords.join(`, `),
-                                }
-                              : []
-                          )}
-                        />
-                      )
-                    }}
-                  />
-                )
-              }
-              
-              const detailsQuery = graphql`
-                query DefaulySEOQuery {
-                  site {
-                    siteMetadata {
-                      title
-                      description
-                      keywords
-                      image
-                    }
-                  }
-                }
-              `
-              
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      link={
+        canonical
+          ? [
+              {
+                rel: "canonical",
+                href: canonical,
+              },
+            ]
+          : []
+      }
+      meta={[
+        {
+          name: "title",
+          content: site.siteMetadata.title,
+        },
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          name: "keywords",
+          content: site.siteMetadata.keywords,
+        },
+        {
+          name: "title",
+          content: site.siteMetadata.siteUrl,
+        },
+      ]}
+    />
+  )
+}
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+export default SEO
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,172 @@
 import React from "react"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container, Row, Col} from 'react-bootstrap'
-import '../styles/style.scss'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Row, Col } from "react-bootstrap"
+import "../styles/style.scss"
 import Portfoliocard from "../components/portfolio-card"
 import Pricingcard from "../components/pricing-card"
-import {FaRegCheckSquare} from 'react-icons/fa'
+import { FaRegCheckSquare } from "react-icons/fa"
 
-
-
-export default function Home({data}) {
+export default function Home({ data }) {
   return (
     <Layout>
       <Container>
         <Row>
           <Col>
-          <Img fluid={data.headerImagetwo.childImageSharp.fluid} alt="Celebrate in Style" />
-          <hr></hr>  
+            <Img
+              fluid={data.headerImagetwo.childImageSharp.fluid}
+              alt="Celebrate in Style"
+            />
+            <hr></hr>
           </Col>
         </Row>
       </Container>
-        <Container>
-        <section style={{'background-color':'#f3f2f2','padding-bottom':'4rem' }}>
-        <Row sm={1} md={2} lg={2}>
-          <Col>
-          <span class="d-flex justify-content-center" style={{color: '#4e2140','font-size':'24px','padding-bottom':'1rem','padding-top':'4rem'}}><FaRegCheckSquare /> </span>
-          <h5 class="d-flex justify-content-center" class="d-flex justify-content-center" style={{'padding-bottom':'1rem'}} >Organic Balloon Garlands</h5>
-          <hr style={{'width':'20%', 'border-width':'5px', 'background-color':'#f4d9e9'}}></hr>
+      <Container>
+        <section
+          style={{ "background-color": "#f3f2f2", "padding-bottom": "4rem" }}
+        >
+          <Row sm={1} md={2} lg={2}>
+            <Col>
+              <span
+                class="d-flex justify-content-center"
+                style={{
+                  color: "#4e2140",
+                  "font-size": "24px",
+                  "padding-bottom": "1rem",
+                  "padding-top": "4rem",
+                }}
+              >
+                <FaRegCheckSquare />{" "}
+              </span>
+              <h5
+                class="d-flex justify-content-center"
+                class="d-flex justify-content-center"
+                style={{ "padding-bottom": "1rem" }}
+              >
+                Organic Balloon Garlands
+              </h5>
+              <hr
+                style={{
+                  width: "20%",
+                  "border-width": "5px",
+                  "background-color": "#f4d9e9",
+                }}
+              ></hr>
+            </Col>
 
-          </Col>
+            <Col>
+              <span
+                class="d-flex justify-content-center"
+                style={{
+                  color: "#4e2140",
+                  "font-size": "24px",
+                  "padding-bottom": "1rem",
+                  "padding-top": "4rem",
+                }}
+              >
+                <FaRegCheckSquare />{" "}
+              </span>
+              <h5
+                class="d-flex justify-content-center"
+                class="d-flex justify-content-center"
+                style={{ "padding-bottom": "1rem" }}
+              >
+                Balloon Walls
+              </h5>
+              <hr
+                style={{
+                  width: "20%",
+                  "border-width": "5px",
+                  "background-color": "#f4d9e9",
+                }}
+              ></hr>
+            </Col>
 
-          <Col>
-          <span class="d-flex justify-content-center" style={{color: '#4e2140','font-size':'24px','padding-bottom':'1rem','padding-top':'4rem'}}><FaRegCheckSquare /> </span>
-          <h5 class="d-flex justify-content-center" class="d-flex justify-content-center" style={{'padding-bottom':'1rem'}} >Balloon Walls</h5>
-          <hr style={{'width':'20%', 'border-width':'5px', 'background-color':'#f4d9e9'}}></hr>
-          </Col>
+            <Col>
+              <span
+                class="d-flex justify-content-center"
+                style={{
+                  color: "#4e2140",
+                  "font-size": "24px",
+                  "padding-bottom": "1rem",
+                  "padding-top": "4rem",
+                }}
+              >
+                <FaRegCheckSquare />{" "}
+              </span>
+              <h5
+                class="d-flex justify-content-center"
+                style={{ "padding-bottom": "1rem" }}
+              >
+                {" "}
+                Balloon Bouquets
+              </h5>
+              <hr
+                style={{
+                  width: "20%",
+                  "border-width": "5px",
+                  "background-color": "#f4d9e9",
+                }}
+              ></hr>
+            </Col>
 
-          <Col>
-          <span class="d-flex justify-content-center" style={{color: '#4e2140','font-size':'24px','padding-bottom':'1rem', 'padding-top':'4rem'}}><FaRegCheckSquare /> </span>
-          <h5 class="d-flex justify-content-center" style={{'padding-bottom':'1rem'}}> Balloon Bouquets</h5>
-          <hr style={{'width':'20%', 'border-width':'5px', 'background-color':'#f4d9e9'}}></hr>
-          </Col>
-
-          <Col>
-          <span class="d-flex justify-content-center" style={{color: '#4e2140','font-size':'24px','padding-bottom':'1rem', 'padding-top':'4rem'}}><FaRegCheckSquare /> </span>
-          <h5 class="d-flex justify-content-center" style={{'padding-bottom':'1rem'}}>Personalized balloons and more</h5>
-          <hr style={{'width':'20%', 'border-width':'5px', 'background-color':'#f4d9e9'}}></hr>
-          </Col>
-
-        </Row>
+            <Col>
+              <span
+                class="d-flex justify-content-center"
+                style={{
+                  color: "#4e2140",
+                  "font-size": "24px",
+                  "padding-bottom": "1rem",
+                  "padding-top": "4rem",
+                }}
+              >
+                <FaRegCheckSquare />{" "}
+              </span>
+              <h5
+                class="d-flex justify-content-center"
+                style={{ "padding-bottom": "1rem" }}
+              >
+                Personalized balloons and more
+              </h5>
+              <hr
+                style={{
+                  width: "20%",
+                  "border-width": "5px",
+                  "background-color": "#f4d9e9",
+                }}
+              ></hr>
+            </Col>
+          </Row>
         </section>
         <hr></hr>
       </Container>
       <section>
-      <Pricingcard />
-      <Portfoliocard />
-      <hr style={{'width':'20%', 'border-width':'5px', 'background-color':'#f4d9e9'}}></hr>
-    </section>  
+        <Pricingcard />
+        <Portfoliocard />
+        <hr
+          style={{
+            width: "20%",
+            "border-width": "5px",
+            "background-color": "#f4d9e9",
+          }}
+        ></hr>
+      </section>
     </Layout>
   )
 }
 export const query = graphql`
   {
-    headerImagetwo: file(relativePath: { eq: "Hiro-Image-Party_Ambiance-Omaha.png" }) {
+    headerImagetwo: file(
+      relativePath: { eq: "Hiro-Image-Party_Ambiance-Omaha.png" }
+    ) {
       childImageSharp {
-        fluid{
+        fluid {
           ...GatsbyImageSharpFluid
         }
       }
     }
-  }  
+  }
 `
 /** 
  *  <Col lg md={10} className="justify-content-md-center">
